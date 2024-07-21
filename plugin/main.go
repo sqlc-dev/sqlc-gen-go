7ec21fe79bc09f65bb179919f90a8c87f31cfa76
Andrii Yanakov <yanak1984@gmail.com>
2024-07-21 13:22:22 +0300

    Add ability to debug teh generation requests

--- plugin/main.go
+++ plugin/main.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"github.com/sqlc-dev/plugin-sdk-go/codegen"
+	"io"
+	"os"
 
 	golang "github.com/sqlc-dev/sqlc-gen-go/internal"
 )
 
 func main() {
+	dumpFile := os.Getenv("DUMP_REQUEST_FILE")
+	restoreFile := os.Getenv("RESTORE_REQUEST_FILE")
+
+	if dumpFile != "" {
+		reqBytes, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.WriteFile(dumpFile, reqBytes, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		fd, err := os.Open(dumpFile)
+		if err != nil {
+			panic(err)
+		}
+		os.Stdin = fd
+	}
+	if restoreFile != "" {
+		fd, err := os.Open(restoreFile)
+		if err != nil {
+			panic(err)
+		}
+		os.Stdin = fd
+	}
+
 	codegen.Run(golang.Generate)
 }
